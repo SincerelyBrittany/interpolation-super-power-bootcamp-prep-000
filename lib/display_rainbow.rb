deb3481c169a598383f1af84b765c2b992eb8bdd
Brittany Russell <sincerelybr@gmail.com>
2020-02-25 19:02:33 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,5 @@
 # Write your #display_rainbow method here
-def display_rainbow(array)
-
+def display_rainbow(color)
+  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  puts color.sort()
 end
